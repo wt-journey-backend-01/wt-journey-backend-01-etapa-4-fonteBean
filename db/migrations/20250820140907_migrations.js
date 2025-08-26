8b3603fec3712bf9b6b078741beee9eb3560c631
fonteBean <n9ckbean@gmail.com>
2025-08-26 19:17:04 -0300

    Add:Agentes, casos  seed

--- db/migrations/20250820140907_migrations.js
+++ db/migrations/20250820140907_migrations.js
@@ -12,7 +12,22 @@ exports.up = function (knex) {
         table.string("name").notNullable();
         table.string("email").unique().notNullable();
         table.string("password").notNullable(); 
-    })
+    }).then(() =>
+      knex.schema.createTable("casos", (table) => {
+        table.increments("id").primary();
+        table.string("titulo").notNullable();
+        table.string("descricao").notNullable();
+        table.string("status").notNullable(); 
+        table.integer("agente_id").references("id").inTable("agentes").nullable().onDelete("set null");
+      })
+    ).then(() =>
+      knex.schema.createTable("agentes", (table) => {
+         table.increments("id").primary();
+          table.string("nome").notNullable();
+          table.date("dataDeIncorporacao").notNullable();
+          table.string("cargo").notNullable();
+      })
+    );
 };
 
 /**
