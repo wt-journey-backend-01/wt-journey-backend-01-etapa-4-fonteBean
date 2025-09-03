174458a6b4fc2b8d6a385730b4ba2e5af8ded441
fonteBean <n9ckbean@gmail.com>
2025-09-03 17:36:21 -0300

    Teste

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,23 +1,19 @@
-const jwt = require("jsonwebtoken");
-require('dotenv').config();
-const errorResponse = require('../utils/errorHandler');
+const jwt = require('jsonwebtoken');
+const errorResponse = require('../utils/errorHandler')
 
-function authMiddleware(req, res, next) {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(" ")[1];
-    if (!token) {
-        return errorResponse(res,404,"Token não fornecido");
+async function authMiddleware(req,res,next){
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if(!token){
+   return   errorResponse(res,401,"Token Necessario");
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+    if(err){
+      return errorResponse(res,401,"Token invalido");
     }
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            console.log(err);
-            return errorResponse(res,401,"Token invalido");
-        }
-        req.user = decoded;
-        next();
-    });
+    req.user = decoded;
+    next();
+  }) 
 }
 
-module.exports = {
-    authMiddleware
-};
+module.exports = authMiddleware;
