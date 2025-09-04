8158d798225e97c67593caf39bce068bb121c664
fonteBean <n9ckbean@gmail.com>
2025-09-04 14:42:40 -0300

    auth change

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,27 +1,19 @@
 const jwt = require('jsonwebtoken');
-const errorResponse = require('../utils/errorHandler');
+const errorResponse = require('../utils/errorHandler')
 
-function authMiddleware(req, res, next) {
-    try {
-        const authHeader = req.headers.authorization;
-        const token = authHeader && authHeader.split(' ')[1];
-        
-        if(!token) {
-            return next(errorResponse(res,'Token não fornecido.', 401));
-        }
-
-        jwt.verify(token, process.env.JWT_SECRET || 'secret', (err, user) => {
-            if(err) {
-                return next(errorResponse(res,'Token inválido ou expirado.', 401));
-            }
-
-            req.user = user;
-            next();
-        });    
-    } 
-    catch (error) {
-        return next(errorResponse(res,'Erro de autenticação de usuário.', 401));
+async function authMiddleware(req,res,next){
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if(!token){
+   return res.status(404).json({"message": "Token nao encontrado"});
+  }
+  jwt.verify(token, process.env.JWT_SECRET || "secret", (err, decoded)=>{
+    if(err){
+      return res.status(401).json({"message": "Token invalido"});
     }
+    req.user = decoded;
+    next();
+  }) 
 }
 
 module.exports = authMiddleware;
